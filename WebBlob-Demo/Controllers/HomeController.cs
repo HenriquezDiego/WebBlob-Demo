1965202f148e183fe5bc8cbf6e3ceb338adbb4e4
Diego Henriquez <df.henriquez@hotmail.com>
2022-05-31 21:12:17 -0600

    version 1, Blob Storage upload and list file.

--- WebBlob-Demo/Controllers/HomeController.cs
+++ WebBlob-Demo/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.WindowsAzure.Storage;
 using System.Diagnostics;
+using Microsoft.WindowsAzure.Storage.Blob;
 using WebBlob_Demo.Models;
 
 namespace WebBlob_Demo.Controllers
@@ -7,17 +9,51 @@ namespace WebBlob_Demo.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _configuration;
+        private readonly List<Image> imagesList;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger,IConfiguration configuration)
         {
             _logger = logger;
+            _configuration = configuration;
+            imagesList = new List<Image>();
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index(string name)
         {
-            return View();
-        }
+            var storageAccount = CloudStorageAccount.Parse(_configuration.GetConnectionString("StorageConnection"));
+            var blobClient = storageAccount.CreateCloudBlobClient();
+            var container = blobClient.GetContainerReference("filepublic");
+            BlobContinuationToken? blobContinuationToken = null;
+            do
+            {
+                var results = await container.ListBlobsSegmentedAsync(null, blobContinuationToken);
+                // Get the value of the continuation token returned by the listing call.
+                blobContinuationToken = results.ContinuationToken;
+                foreach (var item in results.Results)
+                {
+                    Console.WriteLine(item.Uri);
+                    imagesList.Add(new Image(item.Uri.Segments[^1], item.Uri.AbsoluteUri));
+                }
+            } while (blobContinuationToken != null);
 
+            return View(imagesList);
+        }
+        
+
+        [HttpPost]
+        public async Task<ActionResult> UploadFile()
+        {
+            var file = Request.Form.Files[0];
+            if (file.Length <= 0) return RedirectToAction("Index");
+            var storageAccount = CloudStorageAccount.Parse(_configuration.GetConnectionString("StorageConnection"));
+            var blobClient = storageAccount.CreateCloudBlobClient();
+            var container = blobClient.GetContainerReference("filepublic");
+            var blockBlob = container.GetBlockBlobReference(file.FileName);
+            //blockBlob.UploadFromStream(file.InputStream);
+            await blockBlob.UploadFromStreamAsync(file.OpenReadStream());
+            return RedirectToAction("Index");
+        }
         public IActionResult Privacy()
         {
             return View();
---
+++ WebBlob-Demo/Models/Image.cs
@@ -0,0 +1,16 @@
+﻿namespace WebBlob_Demo.Models
+{
+    public class Image
+    {
+        public string Nombre { get; set; }
+        public string UrlImg { get; set; }
+
+        public Image(string nombre, string urlimg)
+        {
+            this.Nombre = nombre;
+            this.UrlImg = urlimg;
+        }
+ 
+      
+    }
+}
